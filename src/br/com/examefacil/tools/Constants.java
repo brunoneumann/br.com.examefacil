bdd1aa2e94f2c1bd7c5425f797418d51c173a8f5
Bruno Neumann <neumannsoft@gmail.com>
2017-10-26 22:45:12 -0200

    adicionado Constants novamente

---
+++ src/br/com/examefacil/tools/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.examefacil.tools;
+
+/**
+ *
+ * @author bruno
+ */
+public class Constants {
+    
+    public Constants(){}
+    
+    public String accessToken = "EAAV4MCWtjiMBALlvWtEnvwWfeLJ3DcosaKFlbDP2i3QmoteGIrGOucnwIz4TMIYt6n6vB8t6MPZCNXMYAT0c9wg4sWEuWaG9LDGDsGzvxkLZBhnUxBSdfZB243EvzwdngCjaggRZAkCMhH8UKlVe4btMn8wmXhJ2EKsQpkJZAtQZDZD";
+    
+}
